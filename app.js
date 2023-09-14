ed23be069702fa7f22cbe396c0e1bd90bb6ccf4d
Donizete Junior <donizete.junior>
2023-09-14 15:55:31 -0300

    insert new user in database

--- app.js
+++ app.js
@@ -15,11 +15,47 @@ app.get('/', (req,res) => {
 
 app.use(express.json())
 
+
+//Models
+const User = require ('./models/User')
+
 //Registrar usuario
     app.post('/auth/register', async(req,res) => {
         const {name, email, password, confirmpassword} = req.body
         if(!name) {
             return res.status(422).json({msg: 'O nome é obrigatório'})
+        }
+        if(!email) {
+            return res.status(422).json({msg: 'O e-mail é obrigatório'})
+        }
+        if(!password) {
+            return res.status(422).json({msg: 'A senha é obrigatória'})
+        }
+        
+        if ( password !== confirmpassword) {
+            return res.status(422).json ( {msg: 'Senhas não conferem!'})
+        }
+
+        const userExists = await User.findOne ({email:email})
+        if (userExists) {
+            return res.status(422).json ({msg: 'email já cadastadro, favor utilize um e-mail válido'})
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const user = new User ({
+            name,
+            email,
+            password,
+        })
+        try{
+            await user.save()
+            res.status(201).json ({msg: 'Usuario cadastro com sucesso!'})
+        } catch(error) {
+           console.log(error)
+           
+            res.status(500).json({msg: 'Algo aconteceu de errado, tente novamente mais tarde!'})
         }
     })
 
