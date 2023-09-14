d6b95911b619afb2250e96f07f3ca60a56cd8696
Donizete Junior <donizete.junior>
2023-09-14 15:35:11 -0300

    Feat: Conection with database

--- app.js
+++ app.js
@@ -11,5 +11,27 @@ app.get('/', (req,res) => {
     res.status(200).json({msg: 'Seja bem vindo!'})
 })
 
+//config JSON
 
-app.listen(3000)
+app.use(express.json())
+
+//Registrar usuario
+    app.post('/auth/register', async(req,res) => {
+        const {name, email, password, confirmpassword} = req.body
+        if(!name) {
+            return res.status(422).json({msg: 'O nome é obrigatório'})
+        }
+    })
+
+
+//Credenciais
+const dbUser= process.env.DB_USER
+const dbPassord= process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassord}@cluster0.acpuzdp.mongodb.net/?retryWrites=true&w=majority`)
+    .then(()=>{
+    app.listen(3000)
+    console.log("conexão com o banco de dados bem sucedido")
+})
+.catch((err)=> console.log(err))
+
---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+
+const user = mongoose.model('User', {
+    name: String,
+    email: String,
+    password:String,
+})
+
+module.exports = user
